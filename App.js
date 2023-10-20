fb9323f07e4095da5e0a872f3b5042b58554a31e
SadeEtensh <eteneshsade550305@gmail.com>
2023-10-20 14:50:22 +0300

    Toggle Switch

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "nativewind";
+import { StyleSheet, Switch, Text, View } from "react-native";
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  console.log(colorScheme);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-cente ">
+      <View className="flex-row justify-center items-center ">
+        <Text className="text-xl">Toggle Theme</Text>
+        <Switch value={colorScheme == "dark"} onChange={toggleColorScheme} />
+      </View>
+      <Text className="text-center">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
